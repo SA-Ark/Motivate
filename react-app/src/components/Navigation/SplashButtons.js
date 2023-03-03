72ecc2a85221b8fbfa1d17c412b9382d8c0a245a
Arko Chakrabarty <c.arko.business@gmail.com>
2023-03-02 16:03:21 -0800

    changed css, updated date

--- react-app/src/components/Navigation/SplashButtons.js
+++ react-app/src/components/Navigation/SplashButtons.js
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../Modals/LoginFormModal";
 import SignupFormModal from "../Modals/SignupFormModal";
-
+import DemoUserButton from "../Buttons/DemoUserButton";
 import { useSelector } from "react-redux";
 
 function SplashButtons() {
@@ -21,6 +21,7 @@ if (!sessionUser){
               // onItemClick={closeMenu}
               modalComponent={<LoginFormModal />}
             />
+            <DemoUserButton/>
            </div>
            <div className="signup">
             <OpenModalButton
