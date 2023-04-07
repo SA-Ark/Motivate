06f405c9e7ff5e8c32d0c6daa888d4cf7bb2c77f
Arko Chakrabarty <c.arko.business@gmail.com>
2023-04-07 13:44:18 -0700

    fixed minor bugs

--- react-app/src/components/Cards/GoalCard.jsx
+++ react-app/src/components/Cards/GoalCard.jsx
@@ -13,13 +13,16 @@ function GoalCard({ goal }) {
       <p>Description: {goal?.description}
 
       </p>
-      <p> {goal?.id}</p>
+
       <p>Difficulty: {goal?.difficulty || "unspecified"}</p>
       <p>Importance: {goal?.importance ||"unspecified" }</p>
       {/* <p>Tags: {goal?.tags || "no tags"}</p> */}
       <p>Due Date: {goal?.due_date || "unspecified"}</p>
+      {goal?.finished_on && <p>Finished On: {goal.finished_on}</p>}
 
-
+      {
+      goal?.finished_on? null:
+      <>
       <div>
         <OpenModalButton
         buttonText="Edit Goal"
@@ -28,17 +31,24 @@ function GoalCard({ goal }) {
         }
         />
       </div>
+
       <div>
+
         <DeleteGoalButton goalId={goal?.id} />
+
       </div>
-      <div>
+
+
+      <CompleteGoalButton goalId={goal?.id}/>
+      </>
+}
+<div>
       <OpenModalButton
       buttonText="View & Update Notes"
         modalComponent={
         <EditGoalNoteModal goalId={goal?.id} />}
         />
       </div>
-      <CompleteGoalButton goalId={goal?.id}/>
     </div>
   )
 }
